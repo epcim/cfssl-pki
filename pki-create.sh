bfbfc00dfbcc8fb94109d830502d27c5c616b55c
Petr Michalec <epcim@apealive.net>
2018-06-27 18:21:53 +0200

    Minor updates in naming...

--- pki-create.sh
+++ pki-create.sh
@@ -13,26 +13,35 @@ if ./pki.sh ca_api POST info "{\"label\": \"\"}" >/dev/null; then
       {
         "C":  "CZ",
         "L":  "Prague",
-        "O":  "Demo",
-        "OU": "Geeks"
+        "O":  "Demo"
       }
     ]'
   ca_update $C/ca
 
   # ----
   # EXAMPLE 1
-  FQDN="etcd-vault.demo.local" CN=${FQDN//.*/} AN="$FQDN $CN 127.0.0.1 $OTHER_ALT_NAME_FOR_ITS_CERT"
-  echo '{"CN":"'$CN'","hosts":['$(join_by , $(enquote $AN))'],"key":{"algo":"rsa","size":2048},'$CERT_NAMES'}' |\
-      cert_gen $C/$CN.pem server /dev/stdin
+  FQDN="vault-etcd.demo.local" ROLE=server CN=${FQDN//.*/} AN="$FQDN $CN 127.0.0.1 $OTHER_ALT_NAME_FOR_ITS_CERT"
+  echo '{"CN":"'$CN'","OU":"'$ROLE'", "hosts":['$(join_by , $(enquote $AN))'],"key":{"algo":"rsa","size":2048},'$CERT_NAMES'}' |\
+      cert_gen $C/$CN-$ROLE.pem $ROLE /dev/stdin
+
+  FQDN="vault-etcd.demo.local" ROLE=peer CN=${FQDN//.*/} AN="$FQDN $CN 127.0.0.1 $OTHER_ALT_NAME_FOR_ITS_CERT"
+  echo '{"CN":"'$CN'","OU":"'$ROLE'","hosts":['$(join_by , $(enquote $AN))'],"key":{"algo":"rsa","size":2048},'$CERT_NAMES'}' |\
+      cert_gen $C/$CN-$ROLE.pem $ROLE /dev/stdin
+
+  FQDN="vault.demo.local" ROLE=client CN=${FQDN//.*/} AN="$FQDN $CN 127.0.0.1 $OTHER_ALT_NAME_FOR_ITS_CERT"
+  echo '{"CN":"'$CN'","OU":"'$ROLE'","hosts":['$(join_by , $(enquote $AN))'],"key":{"algo":"rsa","size":2048},'$CERT_NAMES'}' |\
+      cert_gen $C/$CN-$ROLE.pem $ROLE /dev/stdin
+
+  FQDN="vault.demo.local" ROLE=server CN=${FQDN//.*/} AN="$FQDN $CN 127.0.0.1 $OTHER_ALT_NAME_FOR_ITS_CERT"
+  echo '{"CN":"'$CN'","OU":"'$ROLE'","hosts":['$(join_by , $(enquote $AN))'],"key":{"algo":"rsa","size":2048},'$CERT_NAMES'}' |\
+      cert_gen $C/$CN-$ROLE.pem $ROLE /dev/stdin
+
+  FQDN="$USER.demo.local" ROLE=client CN=${FQDN//.*/} AN="$FQDN $CN 127.0.0.1 $OTHER_ALT_NAME_FOR_ITS_CERT"
+  echo '{"CN":"'$CN'","OU":"'$ROLE'","hosts":['$(join_by , $(enquote $AN))'],"key":{"algo":"rsa","size":2048},'$CERT_NAMES'}' |\
+      cert_gen $C/$CN-$ROLE.pem $ROLE /dev/stdin
 
   # ----
-  # EXAMPLE 2
-  FQDN="vault.demo.local" CN=${FQDN//.*/} AN="$FQDN $CN 127.0.0.1 $OTHER_ALT_NAME_FOR_ITS_CERT"
-  echo '{"CN":"'$CN'","hosts":['$(join_by , $(enquote $AN))'],"key":{"algo":"rsa","size":2048},'$CERT_NAMES'}' |\
-      cert_gen $C/$CN.pem client /dev/stdin
-
-  # ----
-  # EXAMPLE3 - locally - direct invocation, no api server
+  # EXAMPLE 2 - locally - direct invocation, no api server
   #pushd $C
   #echo '{"CN":"'$CRTCN'","hosts":[""],"key":{"algo":"rsa","size":2048}}' |\
   #  cfssl gencert -ca=$C/ca.pem -ca-key=$C/ca-key.pem -config=$C/config.json -profile=server \
